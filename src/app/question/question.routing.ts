8c56edc76f2ac7e4ec90944c1b2259217fb0c228
Mariano Villarreal <marianov994@gmail.com>
2020-11-06 20:34:28 -0300

    refator/remove useless libraries

--- src/app/question/question.routing.ts
+++
@@ -1,9 +0,0 @@
-import { QuestionListComponent } from './question-list.component';
-import { QuestionDetailComponent } from './question-detail.component';
-import { QuestionFormComponent } from './question-form.component';
-
-export const QUESTION_ROUTES = [
-  { path: '', component: QuestionListComponent },
-  { path: 'add', component: QuestionFormComponent },
-  { path: ':id', component: QuestionDetailComponent }
-];
